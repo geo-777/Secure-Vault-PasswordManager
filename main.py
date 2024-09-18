feb54ee9de41f199bd220282933944e93b20c0f8
Georgy Binu <geo.binu.25@gmail.com>
2024-09-18 14:28:14 +0530

    V1.0

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from encryption import encrypt,decrypt
 import os
 from passgen import * #importing password generator , strength checker
 import pyfiglet as ascii_art_generator # this module is used for generating ascii arts
+import pwinput #used to hide inputted text
+import tabulate #used for grid representation 
+
+# Installations
+# pip install tabulate
+# pip install pwinput
+# pip install cryptography
+# pip install mysql-connector-python
+# pip install pyfiglet
+
 # Defining colors using ANSI Escape codes
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -20,7 +30,7 @@ menu_text="""MENU
 1) Store a password         2) Load a password
 3) View all Passwords       4) Delete a password
 5) Update a password        6) Password Generator
-7) Password Strenth Checker 8) Exit 
+7) Password Strength Checker 8) Exit 
 
           """
 try:
@@ -66,7 +76,7 @@ def login_screen():
             if data==[]:
                  print(f"{RED}Invalid username.{RESET}\n")
             else: 
-                password=input("Password : ")
+                password = pwinput.pwinput("Password : ", mask='*')
                 #checking if the decrypted value from database and the inputted password matches.
                 if decrypt(data[0][1])==password:
                     print(f"\n{GREEN}Login Succesful{RESET}")
@@ -85,7 +95,7 @@ def login_screen():
             cur=con.cursor()
             cur.execute(f"select * from users where username='{username}'")
             if cur.fetchall()==[]:#this is done to ensure the uniqueness of usernames
-                password=input("Password : ")
+                password = pwinput.pwinput("Password : ", mask='*')
     
                 cur.execute(f"insert into users values('{username}','{encrypt(password)}')")
                 #a respective table is created for the user
@@ -124,11 +134,149 @@ def menu():
 
         #edit here valo
         if choice==1:
-            pass
+            con=connector.connect(host="localhost",
+                user="root",
+                charset="utf8",
+                password="password",
+                database=db_name)
+            cur = con.cursor()
+            site = input("Enter the app or website associated with the password : ")
+            site = site.lower()
+            username_site =input("Enter username : ")
+            pass_site = pwinput.pwinput("Enter password : ", mask='*')
+            pass_fernet = encrypt(pass_site)
+            cur.execute(f"insert into {user} values('{site}','{username_site}','{pass_fernet}')")
+            con.commit()
+            print("Password successfully stored")
+            print()
+            print()    
+            con.commit()
+            cur.close()
+            con.close()           
         elif choice==2:
-            pass
+            con=connector.connect(host="localhost",
+                    user="root",
+                    charset="utf8",
+                    password="password",
+                    database=db_name)
+            cur = con.cursor()
+            search = input("Enter Website/Application name : ")
+            search = search.lower()
+            cur.execute(f"select * from {user} where website like '{search.lower()}'")
+            data = cur.fetchall()
+            if data == []:
+                print("Login details for ",search," not found")
+            elif len(data)>1:
+                print("You have multiple accounts on this Application/Website")
+                for i in data:
+                    print("USERNAME : ",i[1])
+                    print("PASSWORD : ",decrypt(i[2]))
+                    print()
+
+            else:
+                for i in data:
+                    print("USERNAME : ",i[1])
+                    print("PASSWORD : ",decrypt(i[2]))
+            print()
+            print()  
+            con.commit()
+            cur.close()
+            con.close()      
         elif choice==3:
-            pass
+            con=connector.connect(host="localhost",
+                    user="root",
+                    charset="utf8",
+                    password="password",
+                    database=db_name)
+            cur = con.cursor()
+            cur.execute(f"select * from {user}")
+            data = cur.fetchall()
+
+            if data == []:
+                print("No usernames or passwords stored yet")
+            else:
+                table_data=[]
+                # Decrypt passwords and prepare the data for tabulation
+                for i in data:
+                    table_data.append([f"{GREEN}{i[0]}{RESET}", i[1], decrypt(i[2])])
+          
+                # Display table with headers using tabulate
+                print(tabulate.tabulate(table_data, headers=[f"{YELLOW}Website{RESET}", f"{YELLOW}Username{RESET}", f"{YELLOW}Password{RESET}"], tablefmt="grid"))
+            print()
+            print()
+            con.commit()
+            cur.close()
+            con.close()
+        elif choice==4:
+            con=connector.connect(host="localhost",
+                    user="root",
+                    charset="utf8",
+                    password="password",
+                    database=db_name)
+            cur = con.cursor()
+            delete = input("Enter Website/Application name for Deletion : ")
+            cur.execute(f"select * from {user} where website = '{delete.lower()}'")
+            data = cur.fetchall()
+            if data == []:
+                print("Login details for ",delete," not found")
+            elif len(data)>1:
+                print("You have multiple accounts on that Application/Website")
+                print()
+                cur.execute(f"select * from {user} where website = '{delete.lower()}'")
+                data = cur.fetchall()
+                for i in data:
+                    print("USERNAMES : ",i[1])
+                print()
+                check_user = input("Enter the username of the account you want to delete : ")
+                cur.execute(f"delete from {user} where username = '{check_user}'")
+                con.commit()
+                cur.close()
+                con.close()
+                print("Deleted successfully")
+            else:
+                cur.execute(f"delete from {user} where website = '{delete.lower()}'")
+                print("Deleted Successfully")
+            con.commit()
+            print()
+            print()
+        elif choice==5:
+            con=connector.connect(host="localhost",
+                    user="root",
+                    charset="utf8",
+                    password="password",
+                    database=db_name)
+            cur = con.cursor()
+            Updation = input("Enter Website/Application name for Updation : ")
+            cur.execute(f"select * from {user} where website = '{Updation.lower()}'")
+            data = cur.fetchall()
+            if data == []:
+                print("Login details for ",Updation," not found")
+            elif len(data)>1:
+                print("You have multiple accounts on that Application/Website")
+                print()
+                cur.execute(f"select * from {user} where website = '{Updation.lower()}'")
+                data = cur.fetchall()
+                for i in data:
+                    print("USERNAMES : ",i[1])
+                print()
+                check_user = input("Enter the username of the account you want to update : ")
+                new_user = input("Enter new username : ")
+                
+                new_pass = pwinput.pwinput("Enter new password : ", mask='*') 
+                cur.execute(f"update {user} set username = '{new_user}',password = '{encrypt(new_pass)}' where username = '{check_user}'")
+                con.commit()
+                print("Successfully Updated")
+            else:
+                new_user = input("Enter new username : ")
+                new_pass = pwinput.pwinput("Enter new password : ", mask='*')
+                cur.execute(f"update {user} set username = '{new_user}',password = '{encrypt(new_pass)}' where website = '{Updation.lower()}'")
+                con.commit()
+                print("Successfully Updated")
+            print()
+            print()      
+            con.commit()
+            cur.close()
+            con.close()     
         elif choice==6:
             
             length=input("Enter the length for your password : ")
